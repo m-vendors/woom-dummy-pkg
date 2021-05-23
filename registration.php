6380ce7e1bd794db63aec473d4a1cff7281e5dcf
M Vendors <m-vendors@protonmail.com>
2021-05-23 21:29:58 +0300

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'woom_DummyPkg',
+    __DIR__
+);
